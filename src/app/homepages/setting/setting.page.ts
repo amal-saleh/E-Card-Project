b99ab934869f639190c3ef65bd837adf3c965e72
MunaMurad <85133595+MunaMurad@users.noreply.github.com>
2021-09-01 02:30:29 +0300

    adding color theme

--- src/app/homepages/setting/setting.page.ts
+++ src/app/homepages/setting/setting.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-setting',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingPage implements OnInit {
 
-  constructor() { }
+  constructor(private renderer:Renderer2) { }
+
+  onToggleColorTheme(event){
+    console.log(event.detail.checked);
+    if (event.detail.checked){
+      this.renderer.setAttribute(document.body,"color-theme",'dark')
+
+  }else{
+    this.renderer.setAttribute(document.body,"color-theme",'light')
+  }
+}
 
   ngOnInit() {
   }
-
-}
+  }
